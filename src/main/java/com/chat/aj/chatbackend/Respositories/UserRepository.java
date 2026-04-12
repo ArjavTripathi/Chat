4e99c5aa6eb79ed13082b805a75dced641b17025
ArjavTripathi <arjavtri@iastate.edu>
2026-04-12 16:29:50 -0500

    User Repository Created

---
+++ src/main/java/com/chat/aj/chatbackend/Respositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.chat.aj.chatbackend.Respositories;
+
+import com.chat.aj.chatbackend.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
