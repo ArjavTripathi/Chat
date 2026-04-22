67b13ab1cb53523c66ce7c5b8d7dfc62eb0742c4
ArjavTripathi <arjavtri@iastate.edu>
2026-04-21 21:35:00 -0500

    Added invite link and group leave handlers

--- src/main/java/com/chat/aj/chatbackend/Controller/RoomController.java
+++ src/main/java/com/chat/aj/chatbackend/Controller/RoomController.java
@@ -1,15 +1,13 @@
 package com.chat.aj.chatbackend.Controller;
 
-import com.chat.aj.chatbackend.DTO.GroupCreationRequest;
-import com.chat.aj.chatbackend.Respositories.ConversationRepository;
-import com.chat.aj.chatbackend.Respositories.GroupRepository;
 import com.chat.aj.chatbackend.Service.Room.ConversationService;
 import com.chat.aj.chatbackend.Service.Room.GroupService;
+import com.chat.aj.chatbackend.Service.Room.RoomInviteService;
 import com.chat.aj.chatbackend.Service.User.UserService;
 import com.chat.aj.chatbackend.entities.Group;
-import com.chat.aj.chatbackend.entities.User;
-import org.apache.coyote.Response;
+import com.chat.aj.chatbackend.entities.GroupInvite;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +20,16 @@ public class RoomController { //Make group, start dm, close dm, delete group,
     private final GroupService groupService;
     private final UserService userService;
     private final ConversationService conversationService;
+    private final RoomInviteService roomInviteService;
+    private final String frontendurl;
 
     @Autowired
-    public RoomController(GroupService groupService, UserService userService, ConversationService conversationService) {
+    public RoomController(GroupService groupService, UserService userService, ConversationService conversationService, RoomInviteService roomInviteService, @Value("${frontend.url}") String frontendurl) {
         this.groupService = groupService;
         this.userService = userService;
         this.conversationService = conversationService;
+        this.roomInviteService = roomInviteService;
+        this.frontendurl = frontendurl;
     }
 
     @PostMapping("/create")
@@ -48,5 +50,16 @@ public class RoomController { //Make group, start dm, close dm, delete group,
         return ResponseEntity.ok("Group deleted");
     }
 
-    @PostMapping("/invites/")
+    @PostMapping("/invites/create")
+    public ResponseEntity<?> createInvite(Principal principal, @RequestParam(required = true) String groupId){
+        GroupInvite invite = roomInviteService.createInvite(groupId);
+        return ResponseEntity.ok(frontendurl + "/i/" + invite.getLink());
+    }
+
+    @PutMapping("/leave")
+    public ResponseEntity<?> leaveGroup(Principal principal, @RequestParam(required = true) String groupId){
+        groupService.leaveGroup(principal.getName(), groupId);
+        return ResponseEntity.ok().build();
+    }
+
 }
